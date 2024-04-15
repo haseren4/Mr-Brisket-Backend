30772fec8a92d1a255ccc5602a9427fd1d4936d4
Robert Walk <haseren4@gmail.com>
2024-04-15 12:25:51 -0400

    Initial commit

---
+++ src/main/java/com/mrbrisket/backend/MrBrisketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mrbrisket.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrBrisketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrBrisketBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrbrisket/backend/MrBrisketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbrisket.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrBrisketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
